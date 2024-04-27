460444a4a2b5bf781793daa8994dbcea97f03e67
echefulouis <echefulouis456@gmail.com>
2024-04-26 21:47:01 -0500

    Add Cloud front

---
+++ 3. s3_bucket_cloudfront/main.tf
@@ -0,0 +1,114 @@
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+#Block to Create S3 Bucket
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = var.bucket_name
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_object" "provision_source_files" {
+  bucket = aws_s3_bucket.my_s3_bucket.id
+
+  for_each     = fileset("website/", "**/*.*")
+  key          = each.value
+  source       = "website/${each.value}"
+  content_type = each.value
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
+  bucket = aws_s3_bucket.my_s3_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "private_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.bucket_ownership]
+
+  bucket = aws_s3_bucket.my_s3_bucket.id
+  acl    = "private"
+}
+
+#come back later
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+#come back to understand why
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "OAI for my s3 bucket"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.my_s3_bucket.bucket_regional_domain_name
+    origin_id                = var.bucket_name
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = var.bucket_name
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Environment = "Cloud Destribution"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.my_s3_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "s3:GetObject"
+        Effect   = "Allow"
+        Resource = "${aws_s3_bucket.my_s3_bucket.arn}/*"
+        Principal = {
+          CanonicalUser = aws_cloudfront_origin_access_identity.origin_access_identity.s3_canonical_user_id
+        }
+      }
+    ]
+  })
+
+}
